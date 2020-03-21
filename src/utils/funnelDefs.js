eb63bea94b7b6fc17686e76dcd7bf9c8caab0de3
Richard Hess <eswat2@gmail.com>
2020-03-21 16:36:54 -0700

    prettier 2.0 formatting

--- src/utils/funnelDefs.js
+++ src/utils/funnelDefs.js
@@ -3,7 +3,7 @@ const funnelDefs = [
   { id: 2, height: 356, width: 100, x: 120, y: 32, drop: 20 },
   { id: 3, height: 312, width: 100, x: 230, y: 54, drop: 20 },
   { id: 4, height: 268, width: 100, x: 340, y: 76, drop: 20 },
-  { id: 5, height: 224, width: 100, x: 450, y: 98, drop: 20 }
+  { id: 5, height: 224, width: 100, x: 450, y: 98, drop: 20 },
 ]
 
 export { funnelDefs }
